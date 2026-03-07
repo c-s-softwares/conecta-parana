985967a553609ffc8d10eecc225d83eee435da73
gabriel-v15-laptop <lokomemog@gmail.com>
2026-03-07 14:57:45 -0300

    docs: create swagger documentation route

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Conecta Paraná API')
+    .setDescription('API do sistema Conecta Paraná')
+    .setVersion('0.1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
 }
-bootstrap();
+
+void bootstrap();
