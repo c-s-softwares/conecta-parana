e7e0ff55ffe681ec375c4deb374e45ed0b47d15c
Seu Prefeito😜 <oibommcg123@gmail.com>
2026-03-23 21:11:34 -0300

    feature: create users, city and tokens table

---
+++ backend/prisma/migrations/20260324000922_create_user_city_token/migration.sql
@@ -0,0 +1,45 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USUARIO');
+
+-- CreateTable
+CREATE TABLE "cities" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+
+    CONSTRAINT "cities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USUARIO',
+    "city_id" INTEGER,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "refresh_tokens" (
+    "id" SERIAL NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "refresh_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "refresh_tokens_token_key" ON "refresh_tokens"("token");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_city_id_fkey" FOREIGN KEY ("city_id") REFERENCES "cities"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "refresh_tokens" ADD CONSTRAINT "refresh_tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
