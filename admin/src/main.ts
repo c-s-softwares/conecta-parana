19d64cf0e4c0c68e6125ccf50057abe914bdd15c
Gabe <contatogabemelo@gmail.com>
2026-05-15 01:12:33 -0300

    feature: remove toast exposure from main.ts

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -1,22 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { environment } from './environments/environment';
-import { ToastService } from './app/core/services/toast.service';
 
-bootstrapApplication(App, appConfig)
-  .then((appRef) => {
-    if (!environment.production) {
-      // eslint-disable-next-line
-      (window as any).__toast = appRef.injector.get(ToastService);
-      console.info(
-        '[dev] ToastService exposto em window.__toast\n' +
-        "  __toast.show('success', 'Mensagem')\n" +
-        "  __toast.show('error', 'Mensagem')\n" +
-        "  __toast.show('warning', 'Mensagem')\n" +
-        "  __toast.show('info', 'Mensagem')\n" +
-        "  __toast.show('error', 'Sem conexão.', { label: 'Tentar novamente', callback: () => location.reload() })",
-      );
-    }
-  })
-  .catch((err) => console.error(err));
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
