c8ecef1b968d29d68673ac5dad5f4829caa20b43
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2026-05-16 23:08:43 -0300

    feat(mobile): wire AuthGate into app bootstrap

--- mobile/lib/main_dev.dart
+++ mobile/lib/main_dev.dart
@@ -1,8 +1,14 @@
 import 'package:conectaparana/app.dart';
+import 'package:conectaparana/core/auth/auth_service.dart';
 import 'package:conectaparana/core/config/environment.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   Environment.initialize(Flavor.dev);
+
+  await AuthService.instance.init();
+
   runApp(const App());
 }
