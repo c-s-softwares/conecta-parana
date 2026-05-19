8be092816d7fbfb22958832b425b62eac80f7981
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2026-05-18 21:26:19 -0300

    feat(mobile): wire ApiClient interceptors on app bootstrap

--- mobile/lib/app.dart
+++ mobile/lib/app.dart
@@ -2,12 +2,15 @@ import 'package:conectaparana/core/auth/auth_gate.dart';
 import 'package:conectaparana/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       title: 'Conecta Paraná',
       theme: AppTheme.light,
--- mobile/lib/main_dev.dart
+++ mobile/lib/main_dev.dart
@@ -1,12 +1,14 @@
 import 'package:conectaparana/app.dart';
 import 'package:conectaparana/core/auth/auth_service.dart';
 import 'package:conectaparana/core/config/environment.dart';
+import 'package:conectaparana/core/network/api_client.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   Environment.initialize(Flavor.dev);
+  ApiClient.instance.init();
 
   await AuthService.instance.init();
 
