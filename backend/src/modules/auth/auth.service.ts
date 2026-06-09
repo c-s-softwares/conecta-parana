45e4d18ccaa00232366784aac413ba5fea1feea9
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2026-06-08 23:54:31 -0300

    style(auth): apply prettier formatting

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
     private readonly prisma: PrismaService,
     private readonly jwt: JwtService,
     private readonly config: ConfigService,
-  ) { }
+  ) {}
 
   async register(dto: RegisterDto) {
     const exists = await this.prisma.client.user.findUnique({
@@ -112,7 +112,7 @@ export class AuthService {
       name: user.name,
       email: user.email,
       role: user.role,
-      city: user.cityId
+      city: user.cityId,
     };
   }
 
