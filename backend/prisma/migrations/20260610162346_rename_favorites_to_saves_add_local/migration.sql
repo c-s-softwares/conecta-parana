77125b5d3087ee68613ef9c1f03b285a90c43f19
p3drojs <pedroantoni630@gmail.com>
2026-06-10 15:47:18 -0300

    db: rename favorite to save and add local_id relation

---
+++ backend/prisma/migrations/20260610162346_rename_favorites_to_saves_add_local/migration.sql
@@ -0,0 +1,31 @@
+ALTER TABLE "favorites" DROP CONSTRAINT "favorites_communicate_id_fkey";
+
+ALTER TABLE "favorites" DROP CONSTRAINT "favorites_event_id_fkey";
+
+ALTER TABLE "favorites" DROP CONSTRAINT "favorites_news_id_fkey";
+
+ALTER TABLE "favorites" DROP CONSTRAINT "favorites_user_id_fkey";
+
+DROP TABLE "favorites";
+
+CREATE TABLE "saves" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" TEXT NOT NULL,
+    "event_id" TEXT,
+    "communicate_id" TEXT,
+    "news_id" TEXT,
+    "local_id" TEXT,
+
+    CONSTRAINT "saves_pkey" PRIMARY KEY ("id")
+);
+
+ALTER TABLE "saves" ADD CONSTRAINT "saves_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "saves" ADD CONSTRAINT "saves_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE "saves" ADD CONSTRAINT "saves_communicate_id_fkey" FOREIGN KEY ("communicate_id") REFERENCES "communicates"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE "saves" ADD CONSTRAINT "saves_news_id_fkey" FOREIGN KEY ("news_id") REFERENCES "news"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE "saves" ADD CONSTRAINT "saves_local_id_fkey" FOREIGN KEY ("local_id") REFERENCES "locals"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
