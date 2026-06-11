7a8cc62b100d3c6fa3c9ea8caf3a4bbe6911a0a0
p3drojs <pedroantoni630@gmail.com>
2026-06-11 09:55:55 -0300

    db: add unique constraints for Like and Save models and migration

--- backend/prisma/migrations/20260610162346_rename_favorites_to_saves_add_local/migration.sql
+++ backend/prisma/migrations/20260610162346_rename_favorites_to_saves_add_local/migration.sql
@@ -1,9 +1,6 @@
 ALTER TABLE "favorites" DROP CONSTRAINT "favorites_communicate_id_fkey";
-
 ALTER TABLE "favorites" DROP CONSTRAINT "favorites_event_id_fkey";
-
 ALTER TABLE "favorites" DROP CONSTRAINT "favorites_news_id_fkey";
-
 ALTER TABLE "favorites" DROP CONSTRAINT "favorites_user_id_fkey";
 
 DROP TABLE "favorites";
@@ -21,11 +18,7 @@ CREATE TABLE "saves" (
 );
 
 ALTER TABLE "saves" ADD CONSTRAINT "saves_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
 ALTER TABLE "saves" ADD CONSTRAINT "saves_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
 ALTER TABLE "saves" ADD CONSTRAINT "saves_communicate_id_fkey" FOREIGN KEY ("communicate_id") REFERENCES "communicates"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
 ALTER TABLE "saves" ADD CONSTRAINT "saves_news_id_fkey" FOREIGN KEY ("news_id") REFERENCES "news"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
 ALTER TABLE "saves" ADD CONSTRAINT "saves_local_id_fkey" FOREIGN KEY ("local_id") REFERENCES "locals"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
