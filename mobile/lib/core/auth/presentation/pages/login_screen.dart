438dfbdc2072cc8c999c9201ef0e7e934fe08b99
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2026-06-11 23:56:25 -0300

    feat(mobile): changed navigator to gorouter

--- mobile/lib/core/auth/presentation/pages/login_screen.dart
+++ mobile/lib/core/auth/presentation/pages/login_screen.dart
@@ -343,7 +343,7 @@ class _LoginScreenState extends State<LoginScreen> {
             children: [
               const Text('Não tem conta? '),
               GestureDetector(
-                onTap: () => Navigator.pushNamed(context, '/register'),
+                onTap: () => context.push(AppRoutes.register),
                 child: const Text(
                   'Criar conta',
                   style: TextStyle(
