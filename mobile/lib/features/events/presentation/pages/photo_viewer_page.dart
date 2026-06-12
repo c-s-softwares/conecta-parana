6ce79b99910e1e4be31689f47e32556b7595fff6
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2026-06-11 23:53:50 -0300

    feat(mobile): add event detail page and fullscreen photo viewer

---
+++ mobile/lib/features/events/presentation/pages/photo_viewer_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+import 'package:conectaparana/features/events/data/models/event_detail_model.dart';
+
+class PhotoViewerPage extends StatefulWidget {
+  final List<EventPhoto> photos;
+  final int initialIndex;
+
+  const PhotoViewerPage({
+    super.key,
+    required this.photos,
+    this.initialIndex = 0,
+  });
+
+  @override
+  State<PhotoViewerPage> createState() => _PhotoViewerPageState();
+}
+
+class _PhotoViewerPageState extends State<PhotoViewerPage> {
+  late int _currentIndex;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = widget.initialIndex;
+    _pageController = PageController(initialPage: widget.initialIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        title: widget.photos.length > 1
+            ? Text(
+                '${_currentIndex + 1} / ${widget.photos.length}',
+                style: const TextStyle(color: Colors.white, fontSize: 16),
+              )
+            : null,
+      ),
+      body: PhotoViewGallery.builder(
+        pageController: _pageController,
+        itemCount: widget.photos.length,
+        onPageChanged: (index) => setState(() => _currentIndex = index),
+        builder: (context, index) {
+          return PhotoViewGalleryPageOptions(
+            imageProvider: NetworkImage(widget.photos[index].url),
+            minScale: PhotoViewComputedScale.contained,
+            maxScale: PhotoViewComputedScale.covered * 3,
+            heroAttributes: PhotoViewHeroAttributes(
+              tag: widget.photos[index].id,
+            ),
+          );
+        },
+        loadingBuilder: (context, event) =>
+            const Center(child: CircularProgressIndicator(color: Colors.white)),
+      ),
+    );
+  }
+}
