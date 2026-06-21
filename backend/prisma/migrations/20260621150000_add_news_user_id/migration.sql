416e36996b9aee1c4c46663f08f121ab0ea6e506
Gabe <contatogabemelo@gmail.com>
2026-06-21 14:54:43 -0300

    chore(backend): add user_id column to news table

---
+++ backend/prisma/migrations/20260621150000_add_news_user_id/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "news" ADD COLUMN "user_id" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "news" ADD CONSTRAINT "news_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
