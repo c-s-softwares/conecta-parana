b948aa990820682682e60677ec188d612ede9c95
Nicoly Fernandes <nycolysennaa@gmail.com>
2026-06-28 19:33:31 -0300

    fix: register interceptors via ApiClient.init in all mains

--- mobile/lib/main_staging.dart
+++ mobile/lib/main_staging.dart
@@ -1,6 +1,7 @@
 import 'package:conectaparana/app.dart';
 import 'package:conectaparana/core/auth/auth_service.dart';
 import 'package:conectaparana/core/config/environment.dart';
+import 'package:conectaparana/core/network/api_client.dart';
 import 'package:conectaparana/core/network/connectivity_service.dart';
 import 'package:conectaparana/core/router/app_router.dart';
 import 'package:flutter/material.dart';
@@ -10,9 +11,10 @@ void main() async {
 
   Environment.initialize(Flavor.staging);
 
+  ApiClient.instance.init();
   await AuthService.instance.init();
   await AppRouter.instance.init();
   await ConnectivityService.instance.init();
-  
+
   runApp(const App());
 }
